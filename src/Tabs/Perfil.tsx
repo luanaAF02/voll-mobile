aa1098b8630434486f131feab949dbd527f2fc9e
luanaAF02 <146375120+luanaAF02@users.noreply.github.com>
2024-05-29 14:51:02 -0300

    botao no perfil
    ele deu certo voltou para pagina de login

--- src/Tabs/Perfil.tsx
+++ src/Tabs/Perfil.tsx
@@ -1,32 +1,41 @@
-import { VStack, Text, ScrollView, Avatar, Divider } from 'native-base'
-import { Titulo } from '../componentes/Titulo'
-import { useEffect, useState } from 'react'
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import { pegarDadosPaciente } from '../servicos/PacienteServico'
-import { Paciente } from '../interfaces/Paciente'
+import React, { useEffect, useState } from 'react';
+import { VStack, Text, ScrollView, Avatar, Divider } from 'native-base';
+import { Titulo } from '../componentes/Titulo';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { pegarDadosPaciente } from '../servicos/PacienteServico';
+import { Paciente } from '../interfaces/Paciente';
+import { Botao } from '../componentes/Botao';
+ 
 
-export default function Perfil() {
-  const [dadosPaciente, setDadosPaciente] = useState({} as Paciente)
-
+export default function Perfil({navigation}: any) {
+  const [dadosPaciente, setDadosPaciente] = useState({} as Paciente);
+  
   useEffect(() => {
-    async function dadosPaciente() {
-      const pacienteId = await AsyncStorage.getItem('pacienteId')
-      if (!pacienteId) return null
+    async function obterDadosPaciente() {
+      const pacienteId = await AsyncStorage.getItem('pacienteId');
+      if (!pacienteId) return;
 
-      const resultado = await pegarDadosPaciente(pacienteId)
+      const resultado = await pegarDadosPaciente(pacienteId);
       if (resultado) {
-        setDadosPaciente(resultado)
-        console.log(resultado)
+        setDadosPaciente(resultado);
+        console.log(resultado);
       }
     }
-    dadosPaciente()
-  })
+    obterDadosPaciente();
+  }, []);
+
+  const deslogar = async () => {
+    await AsyncStorage.removeItem('token');
+    await AsyncStorage.removeItem('pacienteId');
+    navigation.replace('Login'); // Redirecionar para a tela de login
+  };
+
   return (
     <ScrollView flex={1}>
       <VStack flex={1} alignItems="center" p={5}>
         <Titulo color="blue.500">Meu Perfil</Titulo>
 
-        <Avatar size="xl" source={{ uri: "https://github.com/robertoSRMJunior.png" }} mt={5} />
+        <Avatar size="xl" source={{ uri: dadosPaciente.imagem }} mt={5} />
 
         <Titulo color="blue.500">Informações pessoais</Titulo>
         <Titulo fontSize="lg" mb={1}>{dadosPaciente.nome}</Titulo>
@@ -35,10 +44,17 @@ export default function Perfil() {
 
         <Divider mt={5} />
 
-        <Titulo color="blue.500" mb={1}>Histórico médico</Titulo>
-        <Text>Bronquite</Text>
-        <Text>Sinusite</Text>
+        <Titulo color="blue.500" mb={1}>Planos de Saúde</Titulo>
+        {
+          dadosPaciente.planosSaude && dadosPaciente.planosSaude.map((plano, index) => (
+            <Text key={index}>{plano}</Text>
+          ))
+        }
+
+        <Botao onPress={deslogar}>
+          deslogar
+        </Botao>
       </VStack>
     </ScrollView>
-  )
+  );
 }
